9107fe19778d716bf1c50ff272dd91cf0de0bcbd
Tom Settle <tom.k.settle@gmail.com>
2023-02-07 17:45:36 -0600

    Mod 3, Selection statements, Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your favorite subject in school...");
+            string faveSubject = Console.ReadLine();
+            string snarkyRemark;
+
+            switch (faveSubject.ToUpper())
+            {
+                case "ENGLISH":
+                    snarkyRemark = "Ohhh, so you're a barista?";
+                    break;
+                case "HISTORY":
+                    snarkyRemark = "Ahh, living in the past then?";
+                    break;
+                case "MATH":
+                case "SCIENCE":
+                case "PHYSICS":
+                case "CHEMISTRY":
+                    snarkyRemark = "You can't really separate math and science, can you? They eventually merge.";
+                    break;
+                case "DRAMA":
+                    snarkyRemark = "Yeah... saw that coming.";
+                    break;
+                default:
+                    snarkyRemark = "Hmmm... I don't know that subject.";
+                    break;
+            }
+            Console.WriteLine(snarkyRemark);
         }
     }
 }
